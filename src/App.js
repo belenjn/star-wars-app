4179e61dba637e596b8a4ac5300a6335f4ee349b
Belen JN <belenjarabanavas31@gmail.com>
2022-09-01 10:31:45 +0200

    Routes solved

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { Footer } from "./components/footer/Footer";
 import { Navbar } from "./components/navbar/Navbar";
 import React from "react";
-import {Route, HashRouter} from "react-router-dom";
+import { Route, HashRouter, Routes } from "react-router-dom";
 import { Movies } from "./pages/movies/Movies";
 import { Planets } from "./pages/planets/Planets";
 import { Starships } from "./pages/starships/Starships";
@@ -12,15 +12,17 @@ import { CharactersGrid } from "./components/grid/characters/CharactersGrid";
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <HashRouter>
-        <Route exact path="/" element={<CharactersGrid />} />
-        <Route path="/movies"  element={<Movies />} />
-        <Route path="/planets" element={<Planets />} />
-        <Route path="/starships" element={<Starships />} />
+        <Navbar />
+
+        <Routes>
+          <Route exact path="/" element={<CharactersGrid />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/planets" element={<Planets />} />
+          <Route path="/starships" element={<Starships />} />
+        </Routes>
+        <Footer />
       </HashRouter>
-      
-      <Footer />
     </div>
   );
 }
